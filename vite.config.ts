8f4525177c548bdc159dd21f9a25886e4f43bb72
Santiago Suescun <116961553+sasamile@users.noreply.github.com>
2024-02-10 16:54:48 -0500

    Actualizacion de Notion

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/Mi-Primera-web-React",
   plugins: [react()],
-  // base :"https://sasamile.github.io/prueba/"
 
   
 
